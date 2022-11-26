d86c062342a78a4fa8f0e846952800b41f140920
DiegoE <64572465+dego137@users.noreply.github.com>
2022-11-25 23:20:28 -0500

    Update StudentRepository.java

--- src/main/java/pe/edu/upc/procv/repository/StudentRepository.java
+++ src/main/java/pe/edu/upc/procv/repository/StudentRepository.java
@@ -7,6 +7,4 @@ import pe.edu.upc.procv.model.Student;
 import java.util.List;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
-   @Query("SELECT dni FROM students s WHERE s.name LIKE %?1%")
-    List<Product> findByNameLike(String name);
 }
